7a275a97f45d05edf9d0e4b8baa87af239d3528a
HappyGri <60741700+HappyGri@users.noreply.github.com>
2022-08-31 23:44:26 +0500

    Add files via upload

---
+++ 12/12/12.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    int x = 0;
+    ++n;
+    ++m;
+    x = n * m;
+    std::cout << "1: ";
+    std::cout << x;
+    std::cout << '\n';
+    x = 0;
+    m = 0;
+    n = 0;
+    m++;
+    x = m < n;
+    std::cout << "2: ";
+    std::cout << x;
+    std::cout << '\n';
+    x = 0;
+    m = 0;
+    n = 0;
+    n++;
+    x = n > m;
+    std::cout << "3: ";
+    std::cout << x;
+    std::cout << '\n';
+    _getch;
+}
